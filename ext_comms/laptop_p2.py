e95affdfa33c55fb584af9147ab0b14592dbc8fe
adinata15 <adinatatan99@gmail.com>
2022-11-02 22:55:54 +0800

    sync laptop relay

--- ext_comms/laptop_p2.py
+++ ext_comms/laptop_p2.py
@@ -109,7 +109,7 @@ class ScannerDelegate(DefaultDelegate):
                 self.SeqID = (self.SeqID + 1) & 0xFF
                 if(correct_pkt):
                     self.char.write(str.encode(ACK))
-                    # print("Gun correct")
+                    print("Gun correct")
                     relay_buffer.put(pkt)
                 else:
                     self.char.write(str.encode(NAK))
@@ -176,10 +176,10 @@ def connection_thread(bluno, char, addr):
                 if bluno.waitForNotifications(2.0):
                     pass
                 else:
-                    # bluno.disconnect()
-                    # print("Bluno " + addr + " disconnected")
-                    # bluno_handshake = False
-                    # bluno, char = connection(addr)
+                    bluno.disconnect()
+                    print("Bluno " + addr + " disconnected")
+                    bluno_handshake = False
+                    bluno, char = connection(addr)
                     pass
             else:
                 bluno_handshake = handshake(bluno, char, addr)
